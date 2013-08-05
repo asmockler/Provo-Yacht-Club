86e60eca38b4dd91ef78a937149583549715b695
asmockler <asmockler@gmail.com>
2013-08-04 21:25:31 -0400

    Cleaned Up
    Shortened hello.rb considerably

--- hello.rb
+++ hello.rb
@@ -14,30 +14,17 @@ configure do
 end
 
 get '/' do
-	@result = settings.mongo_db["testcollection"]
-	@results = @result.find()
+	@results = settings.mongo_db["testcollection"].find
 
 	erb :index
  
 end
 
 post '/' do
-	name = params[:name]
-	address = params[:address]
-	hospital = params[:hospital]
-	city = params[:city]
-	state = params[:state]
-	zip = params[:zip]
-	repunits = params[:repunits]
-	actunits = params[:actunits]
-	funits = params[:funits]
+	params[:entrytime] = Time.new.strftime("%I:%M%p %Z on %A, %B %d, %Y")
+	id = coll.insert params 
 
-	coll = settings.mongo_db["testcollection"]
-	doc = {"name" => name, "address" => address, "hospital" => hospital, "city" => city, "state" => state, "zip" => zip, "repunits" => repunits, "actunits" => actunits, "funits" => funits, "entrytime" => Time.new.strftime("%I:%M%p %Z on %A, %B %d, %Y")}
-	id = coll.insert(doc)
-
-	@result = settings.mongo_db["testcollection"]
-	@results = @result.find()
+	@results = settings.mongo_db["testcollection"].find
 
 	erb :index
 end
@@ -46,7 +33,7 @@ end
 #  *Add Delete Button (db.testcollection.remove( { hospital: "IUPUI" } , 1 )) 
 #  *Add Update Button
 #  *Sort Abilities
-#  *Make Collapsable?
+#.  *Make Collapsable?
 #  *Restyle to Taste (make "in-progress" checkbox which determines color of panel)
 #  !!!!Expand to handle recall!!!!
 
