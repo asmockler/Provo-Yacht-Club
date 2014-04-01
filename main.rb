28ccda39d08f0ab91549af3251004a7300a0b5a4
asmockler <asmockler@gmail.com>
2014-03-31 19:22:33 -0600

    Login and Cleanup
    Added Login for post stuff. Took out all the dumb old stuff I didn’t
    need.

--- main.rb
+++ main.rb
@@ -16,118 +16,136 @@ configure do
  set :mongo_db, conn
 end
 
+
 # Calling the main view
-get '/' do
-  @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).limit(3)
-  @recentposts = settings.mongo_db["Posts"].find.sort('_id','descending').limit(6)
-  erb :index
-end
+  get '/' do
+    @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).limit(3)
+    @recentposts = settings.mongo_db["Posts"].find.sort('_id','descending').limit(6)
+    erb :index
+  end
 
-get '/moreResults/:batch' do |batch|
-  num_to_skip = 3 * batch.to_i
-  @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).skip(num_to_skip).limit(3)
-  erb :posts_long_front
-end
+  get '/moreResults/:batch' do |batch|
+    num_to_skip = 3 * batch.to_i
+    @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).skip(num_to_skip).limit(3)
+    erb :posts_long_front
+  end
 
-# Sorting
-get '/Big Beats' do
-  @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Big Beats"}, {:tag2 => "Big Beats"}] }).sort({_id: -1}).limit(3)
-  @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Big Beats"}, {:tag2 => "Big Beats"}] }).limit(6)
-  erb :index
-end
+      # Sorting
+      get '/Big Beats' do
+        @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Big Beats"}, {:tag2 => "Big Beats"}] }).sort({_id: -1}).limit(3)
+        @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Big Beats"}, {:tag2 => "Big Beats"}] }).limit(6)
+        erb :index
+      end
 
-get '/Dance' do
-  @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Dance"}, {:tag2 => "Dance"}] }).sort({_id: -1}).limit(3)
-  @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Dance"}, {:tag2 => "Dance"}] }).sort('_id','descending').limit(6)
-  erb :index
-end
+      get '/Dance' do
+        @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Dance"}, {:tag2 => "Dance"}] }).sort({_id: -1}).limit(3)
+        @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Dance"}, {:tag2 => "Dance"}] }).sort('_id','descending').limit(6)
+        erb :index
+      end
 
-get '/Chill' do
-  @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Chill"}, {:tag2 => "Chill"}] }).sort({_id: -1}).limit(3)
-  @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Chill"}, {:tag2 => "Chill"}] }).sort('_id','descending').limit(6)
-  erb :index
-end
+      get '/Chill' do
+        @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Chill"}, {:tag2 => "Chill"}] }).sort({_id: -1}).limit(3)
+        @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Chill"}, {:tag2 => "Chill"}] }).sort('_id','descending').limit(6)
+        erb :index
+      end
 
-get '/Revival' do
-  @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Revival"}, {:tag2 => "Revival"}] }).sort({_id: -1}).limit(3)
-  @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Revival"}, {:tag2 => "Revival"}] }).sort('_id','descending').limit(6)
-  erb :index
-end
+      get '/Revival' do
+        @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Revival"}, {:tag2 => "Revival"}] }).sort({_id: -1}).limit(3)
+        @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Revival"}, {:tag2 => "Revival"}] }).sort('_id','descending').limit(6)
+        erb :index
+      end
 
-get '/Etc.' do
-  @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Etc."}, {:tag2 => "Etc."}] }).sort({_id: -1}).limit(3)
-  @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Etc."}, {:tag2 => "Etc."}] }).sort('_id','descending').limit(6)
-  erb :index
-end
+      get '/Etc.' do
+        @posts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Etc."}, {:tag2 => "Etc."}] }).sort({_id: -1}).limit(3)
+        @recentposts = settings.mongo_db["Posts"].find({ :$or => [{:tag => "Etc."}, {:tag2 => "Etc."}] }).sort('_id','descending').limit(6)
+        erb :index
+      end
 
-# A Page for dumb experiments because I'm dumb
-get '/testingstuff' do
-  erb :testtime
-end
+
 
 # Calling the Post Manager
-get '/Manager' do
-  @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).limit(5)
-  erb :Manager
-end
-
-# Saving new posts
-post '/NewPost' do
-  params[:entrytime] = Time.new.strftime("%I:%M%p %Z on %A, %B %d, %Y")
-  settings.mongo_db["Posts"].insert params
-
-  if settings.mongo_db['Posts'].find({:_id => params[:id]})
-    flash[:postSuccess] = true
-  else
-    flash[:postError] = true
+  get '/Manager' do
+    if session["user"]
+     @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).limit(5)
+     erb :Manager
+    else
+     redirect '/login'
+    end
   end
 
-  redirect '/Manager'
-end
+      # Saving new posts
+      post '/NewPost' do
+        params[:entrytime] = Time.new.strftime("%I:%M%p %Z on %A, %B %d, %Y")
+        settings.mongo_db["Posts"].insert params
 
-# Delete a record
-get '/delete/:id' do
-  id = BSON::ObjectId.from_string(params[:id])
-  settings.mongo_db["Posts"].remove({:_id => id})
+        if settings.mongo_db['Posts'].find({:_id => params[:id]})
+          flash[:postSuccess] = true
+        else
+          flash[:postError] = true
+        end
 
-  if settings.mongo_db['Posts'].remove({:_id => id})
-    flash[:deleteSuccess] = true
-  else
-    flash[:deleteError] = true
+        redirect '/Manager'
+      end
+
+      # Delete a record
+      get '/delete/:id' do
+        id = BSON::ObjectId.from_string(params[:id])
+        settings.mongo_db["Posts"].remove({:_id => id})
+
+        if settings.mongo_db['Posts'].remove({:_id => id})
+          flash[:deleteSuccess] = true
+        else
+          flash[:deleteError] = true
+        end
+
+        redirect '/Manager'
+      end
+
+      # Submit edited entry
+      post '/edit/:id' do
+        @id = BSON::ObjectId.from_string(params[:id])
+        settings.mongo_db["Posts"].update({:_id => @id}, params )
+
+        if settings.mongo_db["Posts"].update({:_id => @id}, params )
+          flash[:editSuccess] = true
+        else
+          flash[:editError] = true
+        end
+
+        redirect '/Manager'
+      end
+
+      get '/Manager/moreResults/:batch' do |batch|
+        num_to_skip = 5 * batch.to_i
+        @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).skip(num_to_skip).limit(5)
+        erb :manage_table
+      end
+
+      get '/Manager/edit-modal/:id' do |id|
+        @post = settings.mongo_db["Posts"].find_one({_id: BSON::ObjectId(id)})
+        erb :manager_edit_form
+      end
+
+      get "/Manager/delete/:id" do |id|
+        @post = settings.mongo_db["Posts"].find_one({_id: BSON::ObjectId(id)})
+        erb :manager_delete_confirm
+      end
+
+
+# Stuff for Logging In
+  get '/login' do
+    erb :login
   end
 
-  redirect '/Manager'
-end
-
-# Submit edited entry
-post '/edit/:id' do
-  @id = BSON::ObjectId.from_string(params[:id])
-  settings.mongo_db["Posts"].update({:_id => @id}, params )
-
-  if settings.mongo_db["Posts"].update({:_id => @id}, params )
-    flash[:editSuccess] = true
-  else
-    flash[:editError] = true
+  post '/login' do
+    if settings.mongo_db["users"].find_one(:username => params[:username]) and settings.mongo_db["users"].find_one(:password => params[:password])
+      session["user"] = true
+      redirect '/Manager'
+    else
+      flash[:loginerror] = true
+      redirect '/login'
+    end
   end
-
-  redirect '/Manager'
-end
-
-get '/Manager/moreResults/:batch' do |batch|
-  num_to_skip = 5 * batch.to_i
-  @posts = settings.mongo_db["Posts"].find().sort({_id: -1}).skip(num_to_skip).limit(5)
-  erb :manage_table
-end
-
-get '/Manager/edit-modal/:id' do |id|
-  @post = settings.mongo_db["Posts"].find_one({_id: BSON::ObjectId(id)})
-  erb :manager_edit_form
-end
-
-get "/Manager/delete/:id" do |id|
-  @post = settings.mongo_db["Posts"].find_one({_id: BSON::ObjectId(id)})
-  erb :manager_delete_confirm
-end
 
 
 =begin
