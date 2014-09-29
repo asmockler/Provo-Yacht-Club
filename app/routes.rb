5907f0b088bb726fb33fcef72782ecca2b1d0d3f
asmockler <asmockler@gmail.com>
2014-09-29 16:35:41 -0600

    Fix Routes
    Forgot to include a bunch.

--- app/routes.rb
+++ app/routes.rb
@@ -1,2 +1,6 @@
 require_relative 'routes/home'
 require_relative 'routes/admin'
+require_relative 'routes/auth'
+require_relative 'routes/extras'
+require_relative 'routes/admin/posts'
+require_relative 'routes/admin/users'
