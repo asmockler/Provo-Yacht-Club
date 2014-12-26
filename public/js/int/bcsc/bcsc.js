7b77c074b7c441f8c1b200070fab3207e310a923
Andy Mockler <asmockler@users.noreply.github.com>
2014-12-26 15:29:51 -0500

    Update bcsc.js

--- public/js/int/bcsc/bcsc.js
+++ public/js/int/bcsc/bcsc.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	  client_id: '91a4f9b982b687d85c9d42e2f4991a09'
 	});
 
-	SC.get('/resolve', { url: 'https://soundcloud.com/andy-mockler/booty-call-snapchat/' }, function (track){
+	SC.get('/resolve', { url: 'https://soundcloud.com/nautilus-provo/booty-call-snapchat/' }, function (track){
 		Track = track;
 
 		SC.stream('/tracks/' + track.id, function (song) {
