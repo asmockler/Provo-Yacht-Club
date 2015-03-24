6227d6e4f892ba1f7bed616f2f248bb69b56b43f
asmockler <asmockler@gmail.com>
2015-03-24 16:47:56 -0600

    Event Over :(

--- app/routes/extras.rb
+++ app/routes/extras.rb
@@ -4,71 +4,77 @@
   end
 end
 
-get "/thelist" do
-	@count = List.count
-	if @count > 149
-		@full = true
-	else
-		@full = false
-	end
-	erb :'special/list'
-end
+# get "/thelist" do
+# 	@count = List.count
+# 	if @count > 149
+# 		@full = true
+# 	else
+# 		@full = false
+# 	end
+# 	erb :'special/list'
+# end
 
-post "/thelist/:email/:name" do
-	unless List.count < 152
-		return 403
-	end
+# post "/thelist/:email/:name" do
+# 	unless List.count < 152
+# 		return 403
+# 	end
 
-	@list = List.new(:name => params["name"], :email => params["email"])
+# 	@list = List.new(:name => params["name"], :email => params["email"])
 
-	@list.save
+# 	@list.save
 
-	if @list.save
-		return 201
-	else
-		return 409
-	end
+# 	if @list.save
+# 		return 201
+# 	else
+# 		return 409
+# 	end
 	
-	return 201
-end
+# 	return 201
+# end
 
-helpers do
-  def protected!
-    return if authorized?
-    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
-    halt 401, "Not authorized\n"
-  end
+# helpers do
+#   def protected!
+#     return if authorized?
+#     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+#     halt 401, "Not authorized\n"
+#   end
 
-  def authorized?
-    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['asmockler', 'thelistlol123']
-  end
-end
+#   def authorized?
+#     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+#     @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['asmockler', 'thelistlol123']
+#   end
+# end
 
-get "/thelist/send" do
-	protected!
+# get "/thelist/send" do
+# 	protected!
 	
-	@partiers = List.find_each()
+# 	@partiers = List.find_each()
 
-	@partiers.each do |partier|
+# 	@partiers.each do |partier|
 
-		Pony.mail({
-		  :to => partier.email,
-		  :via => :smtp,
-		  :via_options => {
-		    :address              => 'smtp.mandrillapp.com',
-		    :port                 => '587',
-		    :enable_starttls_auto => true,
-		    :user_name            => 'app17320251@heroku.com',
-		    :password             => 'eV3Y8KDvRNvZKMITgQhoww',
-		    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-		    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-		  },
-		  :from => "noreply@provoyachtclub.com",
-		  :html_body => erb(:'special/thelist_email'),
+# 		Pony.mail({
+# 		  :to => partier.email,
+# 		  :via => :smtp,
+# 		  :via_options => {
+# 		    :address              => 'smtp.mandrillapp.com',
+# 		    :port                 => '587',
+# 		    :enable_starttls_auto => true,
+# 		    :user_name            => 'app17320251@heroku.com',
+# 		    :password             => 'eV3Y8KDvRNvZKMITgQhoww',
+# 		    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+# 		    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+# 		  },
+# 		  :from => "noreply@provoyachtclub.com",
+# 		  :html_body => erb(:'special/thelist_email'),
 
-		  :subject => 'THE LIST | Provo Yacht Club'
-		});
+# 		  :subject => 'THE LIST | Provo Yacht Club'
+# 		});
 
-	end
-end
+# 	end
+# end
+
+# get '/thelist/print' do
+# 	@partiers = List.find_each(:order => :name)
+# 	erb :'special/print_thelist'
+# end
+
