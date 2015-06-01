0fab86686def5d88959dfff6cf037d0bf70e2a4f
asmockler <asmockler@gmail.com>
2015-06-01 03:01:29 -0600

    Fix 2nd Song Problems

--- public/js/int/index/index.js
+++ public/js/int/index/index.js
@@ -286,9 +286,15 @@ $(document).ready(function(){
 		var firstSongNumber = $('.song-thumb').first().attr('data-number');
 		var totalSongs = $('body').attr('data-total-songs');
 		if ( parseInt(firstSongNumber, 10) < parseInt(totalSongs, 10) ) {
-			var firstSong = $('.song-thumb:nth-of-type(4)');
-			$('.load-previous').css({'display' : 'inline-block'});
-			$('.song-thumb-row').scrollLeft(176*3 + 1)
+			if ( window.location.pathname.split('/')[2] == $('.song-thumb').eq(0).attr('data-slug') ) {
+				var firstSong = $('.song-thumb:nth-of-type(2)');
+				$('.load-previous').css({'display' : 'inline-block'});
+				$('.song-thumb-row').scrollLeft(176 + 1)
+			} else {			
+				var firstSong = $('.song-thumb:nth-of-type(4)');
+				$('.load-previous').css({'display' : 'inline-block'});
+				$('.song-thumb-row').scrollLeft(176*3 + 1)
+			}
 		} else {
 			var firstSong = $('.song-thumb').first();
 			$('.load-previous').hide();
